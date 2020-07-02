e6d6211854b539f3960e5a44a9722408706eb161
vivek211996 <67255596+vivek211996@users.noreply.github.com>
2020-07-03 04:27:40 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
+
+
 var div1= document.createElement('div')
 div1.setAttribute('class','input-group')
 document.body.append(div1)
 var input1 = document.createElement('input')
 input1.setAttribute('class','form-control')
-input1.setAttribute('placeholder','')
+input1.setAttribute('placeholder','Enter movie name to search')
 input1.setAttribute('aria-label','')
 input1.setAttribute('aria-describedby','basic-addon1')
 input1.setAttribute('id','ip')
@@ -33,9 +35,9 @@ async function moviesearch() {
 					//console.log(two)
 
 				   var mlink = await fetch(one+two)
-				  console.log(mlink)
+				  //console.log(mlink)
 				  let movies = await mlink.json()
-				  console.log(movies)
+				  //console.log(movies)
 				  var b = document.createElement('label')
 				  b.innerHTML = "REFRESH PAGE FOR NEW SEARCH"
 				  document.body.append(b)
@@ -64,7 +66,8 @@ async function moviesearch() {
 								cheader.classList.add('card-header','text-center')
 								cheader.innerHTML =movies.Search[n].Title
 								card.appendChild(cheader)
-				
+								moviename=movies.Search[n].Title;
+
 								var cardBody = document.createElement('div')
 								cardBody.classList.add('card-body', 'card-bg', 'text-center')
 								card.appendChild(cardBody)
@@ -81,7 +84,14 @@ async function moviesearch() {
 								
 				
 								cardBody.append(year)
-	
+
+								var rating= document.createElement('input')
+								rating.setAttribute('type','button')  
+								rating.setAttribute('class','btn btn-primary')
+								rating.setAttribute('value','show IMDb rating')
+								rating.setAttribute('id',''+ moviename)
+								rating.setAttribute('onclick',"showrating(this)")
+								cardBody.append(rating)
 								
 								n++
 								row.appendChild(col)
@@ -96,4 +106,28 @@ async function moviesearch() {
 					  console.log('fetch failed', err);
 					 }
 					}
+					function showrating (moviename){
+						var two=moviename.id;
+						//alert(cityName.id);
+						
+						var one = "https://www.omdbapi.com/?t="
+					var three = "&apikey=bdcc0d76"
+				
+						fetch(one+two+three)
+						.then(function (response)
+						{
+							return response.json()
+						})
+						.then(function(result)
+						{
+							//console.log(result)
+							swal("Imdb rating: "+ result.imdbRating + "\n Cast: " + result.Actors + "\n Awards :"+result.Awards)
+						}) 
+					
+					}
+
+
+					
+					
+					
 					
